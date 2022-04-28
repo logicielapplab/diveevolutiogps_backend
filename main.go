d8e9d3b5ff51f8c702f4888900fe4ea793c1bca3
Jhonatan <jhonatan.quihuiri@gmail.com>
2022-04-27 21:47:58 -0500

    Index data correction 2

--- main.go
+++ main.go
@@ -4,14 +4,19 @@ import (
 	"diveEvolution/routes"
 	"github.com/gorilla/mux"
 	"net/http"
+	"os"
 )
 
 func main(){
 	router := mux.NewRouter()
+	router.HandleFunc("/", Home).Methods("GET")
 	router.HandleFunc("/getIndex", routes.IndexHandler).Methods("GET")
 	router.HandleFunc("/getHeader", routes.HeaderHandler).Methods("GET")
 	router.HandleFunc("/getFooter", routes.FooterHandler).Methods("GET")
 	router.HandleFunc("/updateIndex", routes.UpdateIndexHandler).Methods("Get")
-	//port := os.Getenv("PORT")
-	http.ListenAndServe(":8080", router)
+	port := os.Getenv("PORT")
+	http.ListenAndServe(":"+port, router)
+}
+func Home (w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("https://dive-evolution.herokuapp.com/getIndex\nhttps://dive-evolution.herokuapp.com/getHeader\nhttps://dive-evolution.herokuapp.com/getFooter"))
 }
