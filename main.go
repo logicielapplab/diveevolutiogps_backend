6b121a0771ac2ec6b1d561f4310d6c0a906d7346
Jhonatan <jhonatan.quihuiri@gmail.com>
2022-04-29 01:22:36 -0500

    /api/.. implemented

--- main.go
+++ main.go
@@ -11,15 +11,15 @@ import (
 func main(){
 	router := mux.NewRouter()
 	router.HandleFunc("/", Home).Methods("GET")
-	router.HandleFunc("/getIndex", routes.IndexHandler).Methods("GET")
-	router.HandleFunc("/getHeader", routes.HeaderHandler).Methods("GET")
-	router.HandleFunc("/getFooter", routes.FooterHandler).Methods("GET")
-	router.HandleFunc("/updateIndex", routes.UpdateIndexHandler).Methods("Get")
+	router.HandleFunc("/api/getIndex", routes.IndexHandler).Methods("GET")
+	router.HandleFunc("/api/getHeader", routes.HeaderHandler).Methods("GET")
+	router.HandleFunc("/api/getFooter", routes.FooterHandler).Methods("GET")
+	router.HandleFunc("/api/updateIndex", routes.UpdateIndexHandler).Methods("Get")
 	methods := handlers.AllowedMethods([]string{"GET", "POST"})
 	origin := handlers.AllowedOrigins([]string{"*"})
 	port := os.Getenv("PORT")
 	http.ListenAndServe(":"+port, handlers.CORS(methods, origin)(router))
 }
 func Home (w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("https://dive-evolution.herokuapp.com/getIndex\nhttps://dive-evolution.herokuapp.com/getHeader\nhttps://dive-evolution.herokuapp.com/getFooter"))
+	w.Write([]byte("https://dive-evolution.herokuapp.com/api/getIndex\nhttps://dive-evolution.herokuapp.com/api/getHeader\nhttps://dive-evolution.herokuapp.com/api/getFooter"))
 }
